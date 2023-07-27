1c0a18cee8f3d4b4e0356a6f340c895d812a24db
Isaac Thompson <isaac@isaact.net>
2023-07-26 23:07:40 -0400

    Implement map

--- src/client/world.ts
+++ src/client/world.ts
@@ -3,26 +3,31 @@ import {Vector3} from "babylonjs";
 
 // A world holds block data
 export class World {
-  blocks: {[x: number]: {[y: number]: {[z: number]: Block}}};
 
-  constructor() {
-    this.blocks = {}
-  }
+  private blocks = new Map<number, Map<number, Map<number, Block>>>;
+
+  constructor() {}
 
   getBlock(x: number, y: number, z: number) {
-    return this.blocks[x][y][z];
+    return this.blocks.get(x)?.get(y)?.get(z);
   }
 
   setBlock(x: number, y: number, z: number, block: Block) {
-    if (this.blocks[x] === undefined) { this.blocks[x] = {}};
-    if (this.blocks[x][y] === undefined) { this.blocks[x][y] = {}};
 
-    this.blocks[x][y][z] = block;
+    // Add keys to dictionary
+    if (!this.blocks.has(x)) {
+      this.blocks.set(x, new Map<number, Map<number, Block>>)
+    };
+    if (!this.blocks.get(x)!.has(y)) {
+      this.blocks.get(x)!.set(y, new Map<number, Block>);
+    }
+
+    this.blocks.get(x)!.get(y)!.set(z, block);
   }
 
   *getIterator(): Generator<[Vector3, Block], any, unknown> {
-    for (const x in this.blocks) {
-      for (const y in this.blocks[x]) {
+    for (const x of this.blocks) {
+      for (const y in this.blocks.get(x)) {
         for (const z in this.blocks[x][y]) {
           yield [new Vector3(x, y, z), this.blocks[x][y][z]];
         }
