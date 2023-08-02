03c0807adec7aa439a6411b1c908bf023cd731b7
Isaac Thompson <shufflesninja@gmail.com>
2023-08-01 23:01:49 -0400

    Remove references to RepeatedVertexMesh from MeshGeneratorChunk

--- src/client/chunk/MeshGeneratorChunk.ts
+++ src/client/chunk/MeshGeneratorChunk.ts
@@ -1,7 +1,6 @@
 import {Block} from "../Block";
 import {BasicChunk} from "./BasicChunk";
 
-import * as Mesh from "../mesh/Mesh";
 import * as Babylon from "babylonjs";
 
 // A MeshGeneratorChunk can generate a Babylon mesh with UV data from its block data.
@@ -12,90 +11,105 @@ export class MeshGeneratorChunk extends BasicChunk {
     const blockIterator = this.getIterator();
     const chunkGlobalCoord = this.coordinate.multiplyByFloats(this.size, this.size, this.size);
 
-    const mesh: Mesh.IMesh = new Mesh.RepeatedVertexMesh();
+    const vertices = new Array<number>;
+    const triangles = new Array<number>;
 
+    let vIndex = 0;
+
+    // TODO: Consolidate the redundant code blocks
+
+    // Generate vertex and triangle data
     for (let [coord, block] of blockIterator) {
 
       const aboveBlock = this.getBlock(coord.x, coord.y + 1, coord.z);
       if (aboveBlock == Block.Air || aboveBlock === undefined) {
 
-        const v1 = mesh.addVertex(coord.x, coord.y + 1, coord.z);
-        const v2 = mesh.addVertex(coord.x + 1, coord.y + 1, coord.z);
-        const v3 = mesh.addVertex(coord.x + 1, coord.y + 1, coord.z + 1);
-        const v4 = mesh.addVertex(coord.x, coord.y + 1, coord.z + 1);
+        vertices.push(coord.x, coord.y + 1, coord.z);
+        vertices.push(coord.x + 1, coord.y + 1, coord.z);
+        vertices.push(coord.x + 1, coord.y + 1, coord.z + 1);
+        vertices.push(coord.x, coord.y + 1, coord.z + 1);
         
-        mesh.addTriangle(v1, v2, v3);
-        mesh.addTriangle(v3, v4, v1);
+        triangles.push(vIndex, vIndex + 1, vIndex + 2);
+        triangles.push(vIndex + 2, vIndex + 3, vIndex);
+
+        vIndex += 4;
       }
 
       const belowBlock = this.getBlock(coord.x, coord.y - 1, coord.z);
       if (belowBlock == Block.Air || belowBlock === undefined) {
 
-        const v1 = mesh.addVertex(coord.x, coord.y, coord.z);
-        const v2 = mesh.addVertex(coord.x, coord.y, coord.z + 1);
-        const v3 = mesh.addVertex(coord.x + 1, coord.y, coord.z + 1);
-        const v4 = mesh.addVertex(coord.x + 1, coord.y, coord.z);
+        vertices.push(coord.x, coord.y, coord.z);
+        vertices.push(coord.x, coord.y, coord.z + 1);
+        vertices.push(coord.x + 1, coord.y, coord.z + 1);
+        vertices.push(coord.x + 1, coord.y, coord.z);
         
-        mesh.addTriangle(v1, v2, v3);
-        mesh.addTriangle(v3, v4, v1);
+        triangles.push(vIndex, vIndex + 1, vIndex + 2);
+        triangles.push(vIndex + 2, vIndex + 3, vIndex);
       }
 
       const frontBlock = this.getBlock(coord.x + 1, coord.y, coord.z);
       if (frontBlock == Block.Air || frontBlock === undefined) {
 
-        const v1 = mesh.addVertex(coord.x + 1, coord.y, coord.z);
-        const v2 = mesh.addVertex(coord.x + 1, coord.y, coord.z + 1);
-        const v3 = mesh.addVertex(coord.x + 1, coord.y + 1, coord.z + 1);
-        const v4 = mesh.addVertex(coord.x + 1, coord.y + 1, coord.z);
+        vertices.push(coord.x + 1, coord.y, coord.z);
+        vertices.push(coord.x + 1, coord.y, coord.z + 1);
+        vertices.push(coord.x + 1, coord.y + 1, coord.z + 1);
+        vertices.push(coord.x + 1, coord.y + 1, coord.z);
         
-        mesh.addTriangle(v1, v2, v3);
-        mesh.addTriangle(v3, v4, v1);
+        triangles.push(vIndex, vIndex + 1, vIndex + 2);
+        triangles.push(vIndex + 2, vIndex + 3, vIndex);
       }
 
       const backBlock = this.getBlock(coord.x - 1, coord.y, coord.z);
       if (backBlock == Block.Air || backBlock === undefined) {
 
-        const v1 = mesh.addVertex(coord.x, coord.y, coord.z);
-        const v2 = mesh.addVertex(coord.x, coord.y + 1, coord.z);
-        const v3 = mesh.addVertex(coord.x, coord.y + 1, coord.z + 1);
-        const v4 = mesh.addVertex(coord.x, coord.y, coord.z + 1);
+        vertices.push(coord.x, coord.y, coord.z);
+        vertices.push(coord.x, coord.y + 1, coord.z);
+        vertices.push(coord.x, coord.y + 1, coord.z + 1);
+        vertices.push(coord.x, coord.y, coord.z + 1);
         
-        mesh.addTriangle(v1, v2, v3);
-        mesh.addTriangle(v3, v4, v1);
+        triangles.push(vIndex, vIndex + 1, vIndex + 2);
+        triangles.push(vIndex + 2, vIndex + 3, vIndex);
       }
 
       const leftBlock = this.getBlock(coord.x, coord.y, coord.z - 1);
       if (leftBlock == Block.Air || leftBlock === undefined) {
 
-        const v1 = mesh.addVertex(coord.x, coord.y, coord.z);
-        const v2 = mesh.addVertex(coord.x + 1, coord.y, coord.z);
-        const v3 = mesh.addVertex(coord.x + 1, coord.y + 1, coord.z);
-        const v4 = mesh.addVertex(coord.x, coord.y + 1, coord.z);
+        vertices.push(coord.x, coord.y, coord.z);
+        vertices.push(coord.x + 1, coord.y, coord.z);
+        vertices.push(coord.x + 1, coord.y + 1, coord.z);
+        vertices.push(coord.x, coord.y + 1, coord.z);
         
-        mesh.addTriangle(v1, v2, v3);
-        mesh.addTriangle(v3, v4, v1);
+        triangles.push(vIndex, vIndex + 1, vIndex + 2);
+        triangles.push(vIndex + 2, vIndex + 3, vIndex);
       }
 
       const rightBlock = this.getBlock(coord.x, coord.y, coord.z + 1);
       if (rightBlock == Block.Air || rightBlock === undefined) {
 
-        const v1 = mesh.addVertex(coord.x, coord.y, coord.z + 1);
-        const v2 = mesh.addVertex(coord.x, coord.y + 1, coord.z + 1);
-        const v3 = mesh.addVertex(coord.x + 1, coord.y + 1, coord.z + 1);
-        const v4 = mesh.addVertex(coord.x + 1, coord.y, coord.z + 1);
+        vertices.push(coord.x, coord.y, coord.z + 1);
+        vertices.push(coord.x, coord.y + 1, coord.z + 1);
+        vertices.push(coord.x + 1, coord.y + 1, coord.z + 1);
+        vertices.push(coord.x + 1, coord.y, coord.z + 1);
         
-        mesh.addTriangle(v1, v2, v3);
-        mesh.addTriangle(v3, v4, v1);
+        triangles.push(vIndex, vIndex + 1, vIndex + 2);
+        triangles.push(vIndex + 2, vIndex + 3, vIndex);
       }
     }
 
-    const builtMesh = mesh.getMesh();
-    builtMesh.position = chunkGlobalCoord;
+    // Generate the actual mesh object
+    const mesh = new Babylon.Mesh("chunk");
+
+    const vertexData = new Babylon.VertexData();
+    vertexData.positions = vertices;
+    vertexData.indices = triangles;
+    vertexData.applyToMesh(mesh);
+
+    mesh.position = chunkGlobalCoord;
 
     const mat = new Babylon.StandardMaterial("stone");
     mat.diffuseTexture = new Babylon.Texture("img/stone.png");
-    builtMesh.material = mat;
+    mesh.material = mat;
 
-    return builtMesh;
+    return mesh;
   }
 }
