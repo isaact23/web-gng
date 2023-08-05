3fb6418c77d2fa36dd3591bdf0457dee77811919
Isaac Thompson <shufflesninja@gmail.com>
2023-08-04 22:02:10 -0400

    Break up init() functions into smaller functions

--- src/client/scene/NoMeshScene.ts
+++ src/client/scene/NoMeshScene.ts
@@ -8,8 +8,11 @@ import { IWorld } from "../world/World";
 // A NoMeshScene is NOT responsible for generating meshes.
 
 export class NoMeshScene implements IScene {
-  private scene : Babylon.Scene | undefined;
+  private canvas: HTMLCanvasElement | undefined;
+  private engine: Babylon.Engine | undefined;
+  private scene: Babylon.Scene | undefined;
 
+  // Initialize the scene completely.
   init(
       view: IView,
       firstPerson = false,
@@ -17,49 +20,24 @@ export class NoMeshScene implements IScene {
       debugMode = false
       ): void
   {
-    const canvas = view.getCanvas();
-    const engine = new Babylon.Engine(canvas, true);
+    this.canvas = view.getCanvas();
+    this.engine = new Babylon.Engine(this.canvas, true);
+    
+    // Set up the scene
+    this._initScene(debugMode);
+    if (this.scene === undefined) throw "Failed to initialize this.scene";
+    this._addCamera(firstPerson, startPosition);
+    this._addLight();
+    this._addEventListeners();
 
-    // Set up scene
-    const scene = new Babylon.Scene(engine);
-    scene.gravity = new Babylon.Vector3(0, -0.5, 0);
-    scene.collisionsEnabled = true;
-    scene.enablePhysics();
-    if (debugMode) {
-      scene.debugLayer.show();
-    }
-    this.scene = scene;
-
+    // Run engine render loop
+    const engine = this.engine;
     const fpsElement = view.getFpsElement();
-    engine.runRenderLoop(function () {
+    const scene = this.scene;
+    this.engine.runRenderLoop(function () {
       scene.render();
       fpsElement.innerHTML = engine.getFps().toFixed();
     });
-    
-    window.addEventListener("resize", function () {
-      engine.resize();
-    });
-
-    // Set up camera
-    const camera = new Babylon.UniversalCamera("camera1", new Babylon.Vector3(0, 0, 0), scene);
-    camera.position = startPosition;
-    if (firstPerson) {
-      camera.ellipsoid = new Babylon.Vector3(.5, 1, .5);
-      camera.applyGravity = true;
-      camera.needMoveForGravity = true;
-      camera.checkCollisions = true;
-      camera.speed = 0.3;
-    }
-    camera.attachControl(canvas, true);
-
-    // Add WASD
-    camera.keysUp.push(87);
-    camera.keysDown.push(83);
-    camera.keysLeft.push(65);
-    camera.keysRight.push(68);
-  
-    const light = new Babylon.HemisphericLight("light", new Babylon.Vector3(-1, 1, 0), scene);
-    light.intensity = 0.7;
   }
 
   // Load geometry for a chunk
@@ -74,5 +52,58 @@ export class NoMeshScene implements IScene {
     for (let mesh of meshes) {
       this.scene?.addMesh(mesh);
     }
+  }
+
+  // Initialize the Babylon scene before adding objects.
+  _initScene(debugMode: boolean = false) {
+    if (this.engine === undefined) throw "Cannot initialize scene because this.engine is undefined";
+    
+    // Set up scene
+    const scene = new Babylon.Scene(this.engine);
+    scene.gravity = new Babylon.Vector3(0, -0.5, 0);
+    scene.collisionsEnabled = true;
+    scene.enablePhysics();
+    if (debugMode) {
+      scene.debugLayer.show();
+    }
+    this.scene = scene;
+  }
+
+  // Add a camera to the scene.
+  _addCamera(firstPerson: boolean, startPosition: Babylon.Vector3) {
+
+    // Set up camera
+    const camera = new Babylon.UniversalCamera("camera1", new Babylon.Vector3(0, 0, 0), this.scene);
+    camera.position = startPosition;
+    if (firstPerson) {
+      camera.ellipsoid = new Babylon.Vector3(.5, 1, .5);
+      camera.applyGravity = true;
+      camera.needMoveForGravity = true;
+      camera.checkCollisions = true;
+      camera.speed = 0.3;
+    }
+    camera.attachControl(this.canvas, true);
+
+    // Add WASD
+    camera.keysUp.push(87);
+    camera.keysDown.push(83);
+    camera.keysLeft.push(65);
+    camera.keysRight.push(68);
+  }
+
+  // Add a light to the scene.
+  _addLight() {
+    const light = new Babylon.HemisphericLight("light", new Babylon.Vector3(-1, 1, 0), scene);
+    light.intensity = 0.7;
+  }
+
+  // Add event listeners to the window.
+  _addEventListeners() {
+    const engine = this.engine;
+    if (engine === undefined) throw "Cannot add event listeners before engine is initialized";
+
+    window.addEventListener("resize", function () {
+      engine.resize();
+    });
   }
 }
