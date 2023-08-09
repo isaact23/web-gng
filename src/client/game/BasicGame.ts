24bfd084ca12158a2b6a84bb24b65dd7cde3e5e2
Isaac Thompson <shufflesninja@gmail.com>
2023-08-09 12:47:07 -0400

    Implement walking around

--- src/client/game/BasicGame.ts
+++ src/client/game/BasicGame.ts
@@ -1,3 +1,5 @@
+// Code from https://www.babylonjs-playground.com/#3EDS3A#22
+
 import * as Babylon from "babylonjs";
 import { Vector3 } from "babylonjs";
 
@@ -10,7 +12,8 @@ import { IPlayer } from "../player/Player";
 
 const GRAVITY = -9.81;
 const MAX_FALL_SPEED = 20;
-const WALK_SPEED = 3;
+const WALK_SPEED = 8;
+const LATERAL_ACCELERATION = 10;
 
 // Run all game logic.
 export class BasicGame implements IGame {
@@ -58,6 +61,8 @@ export class BasicGame implements IGame {
 
   // Add a local player controller to the game.
   _addLocalPlayer(player: IPlayer): void {
+    const canvas = this.view.getCanvas();
+
     const capsule = Babylon.MeshBuilder.CreateCapsule("playerCapsule", {
       radius: 0.5,
       height: 2
@@ -66,24 +71,144 @@ export class BasicGame implements IGame {
 
     const camera = new Babylon.UniversalCamera(
       "playerCamera",
-      player.getPosition().add(new Vector3(0, 0, -20)),
+      player.getPosition(),
       this.scene);
+    camera.attachControl(canvas, false);
+    camera.minZ = 0;
+    
+    const input = {
+      forward: false,
+      back: false,
+      left: false,
+      right: false,
+      jump: false
+    };
+
+    this.scene.onKeyboardObservable.add((kbInfo) => {
+      switch (kbInfo.type) {
+        case Babylon.KeyboardEventTypes.KEYDOWN: {
+          switch (kbInfo.event.key) {
+            case 'w':
+            case 'W': {
+              input.forward = true;
+              break;
+            }
+            case 'a':
+            case 'A': {
+              input.left = true;
+              break;
+            }
+            case 's':
+            case 'S': {
+              input.back = true;
+              break;
+            }
+            case 'd':
+            case 'D': {
+              input.right = true;
+              break;
+            }
+            case ' ': {
+              input.jump = true;
+              break;
+            }
+          }
+          break;
+        }
+        case Babylon.KeyboardEventTypes.KEYUP: {
+          switch (kbInfo.event.key) {
+            case 'w':
+            case 'W': {
+              input.forward = false;
+              break;
+            }
+            case 'a':
+            case 'A': {
+              input.left = false;
+              break;
+            }
+            case 's':
+            case 'S': {
+              input.back = false;
+              break;
+            }
+            case 'd':
+            case 'D': {
+              input.right = false;
+              break;
+            }
+            case ' ': {
+              input.jump = false;
+              break;
+            }
+          }
+          break;
+        }
+      }
+    });
+
+    let velX = 0;
+    let velZ = 0;
 
     this.scene.registerBeforeRender(() => {
+
       const fps = this.engine.getFps();
-
-      const oldVel = player.getVelocity();
-      const velDelta = new Vector3(0, GRAVITY / fps, 0);
-      let newVel = oldVel.add(velDelta);
-      if (newVel.y < -MAX_FALL_SPEED) {
-        newVel = new Vector3(0, -MAX_FALL_SPEED, 0);
+  
+      // Calculate gravity
+      const oldVelY = player.getVelocity().y;
+      const gravityAccel = GRAVITY / fps;
+      let newVelY = oldVelY + gravityAccel;
+      if (newVelY < -MAX_FALL_SPEED) {
+        newVelY = -MAX_FALL_SPEED;
       }
-      player.setVelocity(newVel);
+  
+      // Change velocity based on player inputs
+      const lateral = LATERAL_ACCELERATION / fps;
+      let targetVelX = 0;
+      let targetVelZ = 0;
+      if (input.forward && !input.back) { velZ += lateral; }
+      else if (input.back && !input.forward) { velZ -= lateral; }
+      else {
+        if (velZ < 0) {
+          velZ += lateral;
+          if (velZ > 0) { velZ = 0; }
+        }
+        else {
+          velZ -= lateral;
+          if (velZ < 0) { velZ = 0; }
+        }
+      }
+      if (input.left && !input.right) { velX -= lateral; }
+      else if (input.right && !input.left) { velX += lateral; }
+      else {
+        if (velX < 0) {
+          velX += lateral;
+          if (velX > 0) { velX = 0; }
+        }
+        else {
+          velX -= lateral;
+          if (velX < 0) { velX = 0; }
+        }
+      }
 
+      if (velX < -1) { velX = -1; }
+      if (velX > 1)  { velX =  1; }
+      if (velZ < -1) { velZ = -1; }
+      if (velZ > 1)  { velZ =  1; }
+
+      let newVel = new Vector3(velX, 0, velZ).multiplyByFloats(WALK_SPEED, 0, WALK_SPEED);
+      newVel = new Vector3(newVel.x, newVelY, newVel.z);
+
+      // Rotate velocity based on camera rotation
+      const viewAngleY = camera.rotation.y;
+      const rotationAxis = Babylon.Matrix.RotationAxis(Babylon.Axis.Y, viewAngleY);
+      const rotatedVel = Babylon.Vector3.TransformCoordinates(newVel, rotationAxis);
+
+      player.setVelocity(rotatedVel);
+  
       const movement = player.getVelocity().divide(new Vector3(fps, fps, fps));
       capsule.moveWithCollisions(movement);
-
-      console.log(player.getVelocity().y);
+      camera.position = capsule.position;
     });
   }
 
@@ -112,8 +237,14 @@ export class BasicGame implements IGame {
   _addEventListeners() {
     const engine = this.engine;
 
+    // Window resize listener
     window.addEventListener("resize", () => {
       engine.resize();
     });
+
+    // Add pointer lock
+    this.scene.onPointerDown = (event) => {
+      engine.enterPointerlock();
+    }
   }
 }
