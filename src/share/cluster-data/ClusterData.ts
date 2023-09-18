2d702a54f9184e581dc4fb8ffddd25273eb182ca
Isaac Thompson <shufflesninja@gmail.com>
2023-09-17 22:17:56 -0400

    Add TSDoc to ClusterData & IClusterData

--- src/share/cluster-data/ClusterData.ts
+++ src/share/cluster-data/ClusterData.ts
@@ -18,7 +18,10 @@ export class ClusterData implements IClusterData {
     this.chunks = new Grid<IChunkData>();
   }
 
-  // Add a new chunk. Replace any chunk in its spot.
+  /**
+   * Add a new chunk. Replace any chunk in its spot.
+   * @param chunk The chunk to add.
+   */
   addChunk(chunk: IChunkData): void {
     if (chunk.getSize() != this.chunkSize) {
       throw "Chunk size is incompatible with world";
@@ -28,12 +31,20 @@ export class ClusterData implements IClusterData {
     this.chunks.set(coord, chunk);
   }
 
-  // Get the chunk at a coordinate
+  /**
+   * Get the chunk at a coordinate.
+   * @param pos The coordinate of the chunk to access.
+   * @returns The chunk at the specified coordinate.
+   */
   getChunk(pos: Vector3): IChunkData | undefined {
     return this.chunks.get(pos);
   }
 
-  // Get the block at an xyz coordinate
+  /**
+   * Get the block at an xyz coordinate.
+   * @param pos The coordinate to access.
+   * @returns The block at the coordinate, or undefined if there is no block.
+   */
   getBlock(pos: Vector3): Block | undefined {
 
     // Get the chunk that the block is in
@@ -46,7 +57,11 @@ export class ClusterData implements IClusterData {
     return chunk.getBlock(blockCoord);
   }
 
-  // Set a block at an xyz coordinate
+  /**
+   * Set a block at an xyz coordinate.
+   * @param pos The coordinate to update.
+   * @param block The block to set at the specified coordinate.
+   */
   setBlock(pos: Vector3, block: Block): void {
 
     // Get the chunk that the block is in
@@ -64,17 +79,28 @@ export class ClusterData implements IClusterData {
     chunk.setBlock(blockCoord, block);
   }
 
-  // Get iterator for all chunks in the world
+  /**
+   * Get iterator for all chunks in the cluster.
+   * @returns An iterator for all chunks in this cluster.
+   */
   *getIterator(): Generator<IChunkData> {
     return this.chunks.getIterator();
   }
 
-  // Get chunk size
+  /**
+   * Get chunk size.
+   * @returns The chunk width in blocks.
+   */
   getChunkSize(): number {
     return this.chunkSize;
   }
 
-  // Convert world coordinates to chunk coordinates
+  // 
+  /**
+   * Convert world coordinates to chunk coordinates
+   * @param pos The position to convert, in world coordinates.
+   * @returns The chunk coordinate.
+   */
   _worldToChunkCoord(pos: Vector3): Vector3 {
     return new Vector3(
       Math.floor(pos.x / this.chunkSize),
@@ -83,7 +109,12 @@ export class ClusterData implements IClusterData {
     );
   }
 
-  // Convert world coordinates to block coordinates within a chunk
+  // 
+  /**
+   * Convert world coordinates to block coordinates within a chunk.
+   * @param pos The position to convert, in chunk coordinates.
+   * @returns The block coordinate.
+   */
   _worldToBlockCoord(pos: Vector3): Vector3 {
     return new Vector3(
       pos.x % this.chunkSize,
--- src/share/cluster-data/IClusterData.ts
+++ src/share/cluster-data/IClusterData.ts
@@ -3,21 +3,42 @@ import { IChunkData } from "./chunk/IChunkData";
 import { Vector3 } from "babylonjs";
 
 export interface IClusterData {
-  // Add a new chunk. Replace any chunk in its spot.
+  /**
+   * Add a new chunk. Replace any chunk in its spot.
+   * @param chunk The chunk to add.
+   */
   addChunk(chunk: IChunkData) : void;
 
-  // Get the chunk at a coordinate
+  /**
+   * Get the chunk at a coordinate.
+   * @param pos The coordinate of the chunk to access.
+   * @returns The chunk at the specified coordinate.
+   */
   getChunk(pos: Vector3) : IChunkData | undefined;
 
-  // Get the block at an xyz coordinate
+  /**
+   * Get the block at an xyz coordinate.
+   * @param pos The coordinate to access.
+   * @returns The block at the coordinate, or undefined if there is no block.
+   */
   getBlock(pos: Vector3) : Block | undefined;
 
-  // Set a block at an xyz coordinate
+  /**
+   * Set a block at an xyz coordinate.
+   * @param pos The coordinate to update.
+   * @param block The block to set at the specified coordinate.
+   */
   setBlock(pos: Vector3, block: Block) : void;
 
-  // Get iterator for all chunks in the world
+  /**
+   * Get iterator for all chunks in the cluster.
+   * @returns An iterator for all chunks in this cluster.
+   */
   getIterator(): Generator<IChunkData>;
   
-  // Get chunk size
+  /**
+   * Get chunk size.
+   * @returns The chunk width in blocks.
+   */
   getChunkSize(): number;
 }
