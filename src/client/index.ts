33eed60cd0aab7c5e5b35de5299db530e7943bb1
Isaac Thompson <shufflesninja@gmail.com>
2024-02-12 23:36:55 -0500

    Prevent double loading on client side

--- src/client/index.ts
+++ src/client/index.ts
@@ -10,9 +10,13 @@ const socket = io();
 // Create view
 const view = new View();
 
+let gotWorld = false;
 console.log("Waiting for world");
+
 socket.on("world", clusterString => {
+    if (gotWorld) return;
     console.log("Got world");
+    gotWorld = true;
     
     // Create game server
     const game = new Game(view, clusterString, DEBUG_MODE);
